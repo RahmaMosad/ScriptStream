0ada8a4efc104cf1087e26913556758c5628845c
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-04-18 08:41:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 });
 
 function validateForm(event) {
-    event.preventDefault(); // Prevents the form from submitting
+    event.preventDefault(); 
 
     const fname = document.getElementById("fname");
     const lname = document.getElementById("lname");
@@ -14,31 +14,29 @@ function validateForm(event) {
     const gender = document.querySelector('input[name="gender"]:checked');
     const birthdate = document.getElementById("birthdate");
 
-    // Validate email
+    
     checkEmail();
 
-    // You can add validations for other fields here
-
-    // If all validations pass, you can submit the form
+   
     document.getElementById("registerPage").submit();
 }
 
 const checkEmail = () => {
     const emailInput = email.value;
 
-    // Check if email is required
+    
     if (!isRequired(emailInput)) {
         alert('Email is required.');
         return;
     }
 
-    // Check if email is valid
+    
     if (!isEmailValid(emailInput)) {
         alert('Please enter a valid email address.');
         return;
     }
 
-    // Email is valid
+    
     alert('Email is valid.');
 };
 
@@ -47,7 +45,7 @@ const isRequired = (value) => {
 };
 
 const isEmailValid = (email) => {
-    // Regular expression to validate email format
+    
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 };
